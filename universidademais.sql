3ab68165e7f52e52d66c15064ab1061d3ed3df1a
Gabriel Melo <gabrielmarquesm@computacao.ufla.br>
2018-02-26 15:42:10 -0300

    Update universidademais.sql

--- universidademais.sql
+++ universidademais.sql
@@ -7,6 +7,39 @@ CREATE TABLE IF NOT EXISTS post (
   name varchar(255) NOT NULL,
   content text NOT NULL,
   img varchar(50) NOT NULL,
-  post_date timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  date timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (id)
 );
+
+
+CREATE TABLE IF NOT EXISTS comments (
+  id int(11) NOT NULL AUTO_INCREMENT,
+  postId int(11) NOT NULL,
+  name varchar(50) DEFAULT 'Anônimo',
+  date timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  comment text NOT NULL,
+  PRIMARY KEY (id),
+  FOREIGN KEY (postId) REFERENCES post(id)
+);
+
+CREATE TABLE IF NOT EXISTS subcomments (
+  id int(11) NOT NULL AUTO_INCREMENT,
+  commentId int(11) NOT NULL,
+  name varchar(50) DEFAULT 'Anônimo',
+  comment text NOT NULL,
+  date timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (id),
+  FOREIGN KEY (commentId) REFERENCES comments(id)
+);
+
+CREATE TABLE IF NOT EXISTS authors (
+  name VARCHAR(50) NOT NULL,
+  bio text NOT NULL,
+  img varchar(50) NOT NULL,
+  linkedin VARCHAR(100) DEFAULT "#",
+  twitter VARCHAR(100) DEFAULT "#",
+  instagram VARCHAR(100) DEFAULT "#",
+  PRIMARY KEY (name)
+);
+
+INSERT INTO authors (name,bio, img, pass) VALUES ("Usuario", "Sua biografia", "usuario.png", MD5('pass'));
